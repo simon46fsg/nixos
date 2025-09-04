0deac336e0ec6bbd39db3795dc0a82dfab8031ca
simon <simon46fsg@gmail.com>
2025-09-04 20:52:12 +0200

    home-manager

--- flake.nix
+++ flake.nix
@@ -1,14 +1,28 @@
 {
-  description = "A very basic flake";
+  description = "NixOs config";
 
   inputs = {
-    nixpkgs.url = "github:NixOs/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-25.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations.simons-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix];
+      modules = [
+        ./configuration.nix
+
+	home-manager.nixosModules.home-manager
+	{
+	  home-manager.useGlobalPkgs = true;
+	  home-manager.useUserPackagers = true;
+	  home-manager.users.simon = import ./home.nix;
+        }
+      ];
     };
   };
 }
---
+++ home.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, ... }:
+
+{
+  # TODO please change the username & home directory to your own
+  home.username = "simon";
+  home.homeDirectory = "/home/simon";
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    nnn # terminal file manager
+
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
+
+    # utils
+    ripgrep # recursively searches directories for a regex pattern
+    fzf # A command-line fuzzy finder
+
+    # misc
+    which
+  ];
+
+  # basic configuration of git, please change to your own
+  programs.git = {
+    enable = true;
+    userName = "simon";
+    userEmail = "simon46fsg@gmail.com";
+  };
+
+  # starship - an customizable prompt for any shell
+  programs.starship = {
+    enable = true;
+    # custom settings
+    settings = {
+      add_newline = false;
+      aws.disabled = true;
+      gcloud.disabled = true;
+      line_break.disabled = true;
+    };
+  };
+
+  # alacritty - a cross-platform, GPU-accelerated terminal emulator
+  programs.alacritty = {
+    enable = true;
+    # custom settings
+    settings = {
+      env.TERM = "xterm-256color";
+      font = {
+        size = 12;
+        draw_bold_text_with_bright_colors = true;
+      };
+      scrolling.multiplier = 5;
+      selection.save_to_clipboard = true;
+    };
+  };
+
+  programs.bash = {
+    enable = true;
+    enableCompletion = true;
+    # TODO add your custom bashrc here
+    bashrcExtra = ''
+      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+    '';
+
+    # set some aliases, feel free to add more or remove some
+    shellAliases = {
+      nr = "nixos-rebuild switch --flake ~/mysystem/nixos#simons-nixos"
+    };
+  };
+
+  home.stateVersion = "25.05";
+}
