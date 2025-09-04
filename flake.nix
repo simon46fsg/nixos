ad248f2305e23106a93ec48c491a341a68361b97
simon <simon46fsg@gmail.com>
2025-09-05 01:23:33 +0200

    fix

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     };
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-unstable";
+      url = "github:nix-community/nixvim";
       # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
       # url = "github:nix-community/nixvim/nixos-25.05";
 
