abed0929de0e95bce4cde15937bf8701491720f8
simon <simon46fsg@gmail.com>
2025-09-11 16:53:56 +0200

    still a test

--- hypr/home-hypr.nix
+++ hypr/home-hypr.nix
@@ -1,14 +1,8 @@
 { pkgs, inputs, ... }: 
-let
-    hyprland-path = builtins.path {
-        path = ./hyprland.conf;
-        name = "hyprland.conf";
-    };
-in
 {
 
     # Optional, hint Electron apps to use Wayland:
     home.sessionVariables.NIXOS_OZONE_WL = "1";
     wayland.windowManager.hyprland.enable = true;
-    wayland.windowManager.hyprland.extraConfig = hyprland-path;
+    wayland.windowManager.hyprland.extraConfig = builtins.readFile ./hyprland.conf;
 }
