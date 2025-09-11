33fd6387e68a71af368e0f692c974d3ce1ae7a3c
simon <simon46fsg@gmail.com>
2025-09-11 19:56:02 +0200

    disabled kde

--- configuration.nix
+++ configuration.nix
@@ -41,8 +41,8 @@
   
   # display manager stuff
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.desktopManager.plasma6.enable = true;
 
   # Enable sound with pipewire.
   services.pulseaudio.enable = false;
