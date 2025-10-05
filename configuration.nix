4979befec098168d653919491da4d541ed709ba2
simon <simon46fsg@gmail.com>
2025-10-05 20:54:02 +0200

    fix

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos-simon"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  #networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   networking.networkmanager.enable = true;
 
