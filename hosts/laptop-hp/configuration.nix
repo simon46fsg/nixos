52c99db81e45d079304e44d5e9c3562ab7077a62
simon <simon46fsg@gmail.com>
2025-10-14 10:36:37 +0200

    setup swapfile

--- hosts/laptop-hp/configuration.nix
+++ hosts/laptop-hp/configuration.nix
@@ -2,6 +2,14 @@
 
 {
   networking.hostName = "laptop-hp"; # Define your hostname.
+  
+  #sleep setup
+  #services.logind.lidSwitch = "suspend-then-hibernate";
+  swapDevices = [{
+    device = "/swapfile";
+    size = 8 * 1024; # 8GB
+  }];
+
   imports =
     [
       ./hardware-configuration.nix
