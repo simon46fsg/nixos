862ead6c70f4d2bc40219540f9f76475ba66c6f0
simon <simon46fsg@gmail.com>
2025-10-14 10:48:42 +0200

    suspend-then-hibernate upon lidclose

--- hosts/laptop-hp/configuration.nix
+++ hosts/laptop-hp/configuration.nix
@@ -4,11 +4,12 @@
   networking.hostName = "laptop-hp"; # Define your hostname.
   
   #sleep setup
-  #services.logind.lidSwitch = "suspend-then-hibernate";
+  services.logind.lidSwitch = "suspend-then-hibernate";
   swapDevices = [{
     device = "/swapfile";
     size = 8 * 1024; # 8GB
   }];
+  boot.initrd.systemd.enable = true;
 
   imports =
     [
